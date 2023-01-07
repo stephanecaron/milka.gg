8ec560d398f08e2b91cdf73d6e6390843d486987
stephanecaron <stephane.caron@groupesavoie.com>
2023-01-06 22:58:48 -0400

    http fix

--- src/actions.js
+++ src/actions.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 axios.defaults.withCredentials=true; 
 export async function getAllPlayers () { 
-    return await axios.get("milkaggbackend-production.up.railway.app/getall")
+    return await axios.get("http://milkaggbackend-production.up.railway.app/getall")
 } 
 
 export async function sendNewPlayer (myNewPlayer) { 
-    return await axios.post("milkaggbackend-production.up.railway.app/post", {data:myNewPlayer})
+    return await axios.post("http://milkaggbackend-production.up.railway.app/post", {data:myNewPlayer})
 } 
 
 export async function deletePlayer (playersid) { 
-    return await axios.delete(`milkaggbackend-production.up.railway.app/delete?id=${playersid}`)
+    return await axios.delete(`http://milkaggbackend-production.up.railway.app/delete?id=${playersid}`)
 } 
