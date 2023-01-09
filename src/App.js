a8a3b920f7b7e27f019a712a929e59c6c885993d
stephanecaron <stephane.caron@groupesavoie.com>
2023-01-09 10:47:13 -0400

    comment out constant characterlist for webhost

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const characterListFetch = async () => {
 function App() {
 
  const [players, setPlayers] = useState([])
- const [realCharacterList, setRealCharacterList] = useState([])
+/*  const [realCharacterList, setRealCharacterList] = useState([]) */
  
  useEffect(() => {
   const fetchData = async () => {
